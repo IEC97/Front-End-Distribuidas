bf0d35ce2b14de5de1812f4d6e4a2b06c5379742
Ivana Cardozo <iva6869@gmail.com>
2023-05-26 20:25:51 -0300

    Agregando carpeta components

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Constants from 'expo-constants'
+import { Text, View } from 'react-native'
+
+const Main = () =>{
+    return(
+        <View style={{marginTop: 5, flexGrow: 1}}>
+            <Text>Rate Repository</Text>
+        </View>
+    )
+}
+
+export default Main
