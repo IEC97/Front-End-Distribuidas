243f615d5f344d151a2dca9ed2c026e68b6ec6d5
Ivana Cardozo <iva6869@gmail.com>
2023-05-28 21:47:47 -0300

    Modificando App

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Main from './src/components/Main.jsx'
+import LogIn from './src/components/LogIn.js'
 
 export default function App() {
-  return <Main/>
+  return <LogIn/>
   
 }
 
