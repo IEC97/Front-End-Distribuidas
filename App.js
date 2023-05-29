35f27b04be649396c0be739bbf9e6dcc3a39f319
Ivana Cardozo <iva6869@gmail.com>
2023-05-29 01:04:15 -0300

    Haciendo el Login

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import Main from './src/components/Main.jsx'
 import LogIn from './src/components/LogIn.js'
 
 export default function App() {
-  return <LogIn/>
-  
+  return (
+
+    <LogIn/>
+  )
 }
 
 
