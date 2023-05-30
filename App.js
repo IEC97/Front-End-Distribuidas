ccac1152451a814ed7ccc3f87947235dd37bb66f
Ivana Cardozo <iva6869@gmail.com>
2023-05-29 22:40:21 -0300

    Eliminando Main.jsx

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import Main from './src/components/Main.jsx'
-import LogIn from './src/components/LogIn.js'
+import React from 'react';
+import LogIn from './src/components/LogIn.js';
 
 export default function App() {
   return (
