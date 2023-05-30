95f1485e191568a42c7528be8b5e255440a89d7e
Ivana Cardozo <iva6869@gmail.com>
2023-05-29 23:29:13 -0300

    Agregando BottomTab

---
+++ src/components/BottomTab.js
@@ -0,0 +1,34 @@
+import {Platform, Text, View} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+const screenOptions = {
+    tabBarShowLabel:false,
+    headerShown:false,
+    tabBarStyle: {
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        left: 0,
+        elevation: 0,
+        height: 60,
+        background: "#fff"
+    }
+}
+
+
+export default function BottomTab() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator tabBar={(props) => <MyTabBar {...props} />}>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+
+
+
