d9b60d2d3e17c32fbc084856db06dedfb6a1575d
Ivana Cardozo <iva6869@gmail.com>
2023-05-31 18:53:33 -0300

    Agregando Details y settings

---
+++ screen/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Details({navigation}){
+    return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text 
+                onPress={() => navigation.navigate('Home')} 
+                style={{fontSize: 26, fontWeight: 'bold'}}>Details</Text>
+        </View>
+    );
+}
