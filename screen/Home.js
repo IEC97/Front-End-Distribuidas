3979773f2bbe95074eb1c56d9d7d90c8d5cb1837
Ivana Cardozo <iva6869@gmail.com>
2023-05-31 20:18:30 -0300

    Probando

--- screen/Home.js
+++ screen/Home.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import * as React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Home({navigation}){
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text 
-                onPress={() => alert('This is the "Home"')} style={{fontSize: 26, fontWeight: 'bold'}}>Home</Text>
+export default function Home({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text
+                onPress={() => alert('This is the "Home" screen.')}
+                style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
         </View>
     );
 }
-
-
-
--- src/components/BottomTab.js
+++ src/components/BottomTab.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function BottomTab(){
+import Home from './screen/Home';
+import Settings from './screen/Settings';
+import Details from './screen/Details';
+
+const homeName='Home';
+const detailsName='Details';
+const settingsName='Settings';
+
+const Tab = createBottomTabNavigator();
+
+function BottomTab(){
     return(
-        <View>
-            <Text></Text>
-        </View>
-    )
+        <NavigationContainer>
+            <Tab.Navigator 
+                initialRouteName={homeName} 
+                screenOptions={({route}) => ({
+                    tabBarIcon: ({focused, color, size}) =>{
+                        let iconName;
+                        let rn = route.name;
+                        if (rn===homeName){
+                            iconName=focused ? 'home' : 'home-outline';
+                        } else if (rn===detailsName){
+                            iconName=focused ? 'list' : 'list-outline';
+                        } else if(rn===settingsName){
+                            iconName=focused ? 'settings' : 'settings-outline';
+                        }
+                        return <Ionicons name={iconName} size={size} color={color}/>;
+                    },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'grey',
+                labelStyle: {paddingBottom: 10, fontSize: 10},
+                style: {padding: 10, height: 70}
+            }}>
+            <Tab.Screen name={homeName} component={Home}/>
+            <Tab.Screen name={detailsName} component={Details}/>
+            <Tab.Screen name={settingsName} component={Settings}/>
+
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
-    
+export default BottomTab;
