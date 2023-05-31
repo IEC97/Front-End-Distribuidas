3828d93ab93521e7d90ca8d31691d1d6dc7882c4
Ivana Cardozo <iva6869@gmail.com>
2023-05-31 20:31:18 -0300

    Modificando

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 import BottomTab from './src/components/BottomTab.js';
 
 export default function App() {
   return (
 
     <BottomTab/>
-  )
+  );
 }
 
 
