0499d95f5b1157500e9dbb250f232c9bab835b51
gianpeccia <giann.peccia@gmail.com>
2023-06-07 18:15:14 -0300

    inicio terminado + fotos diseño

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { View, StyleSheet, Text, Image, Dimensions, TextInput, TouchableOpacity} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import rodillo from '../imagen/rodillo.png';
 
 const Login = () => {
     const [nickname, setNickname] = useState('');
@@ -9,45 +10,42 @@ const Login = () => {
     const navigation = useNavigation();
   
     const handleLogin = async () => {
-      // Realizar solicitud al backend para verificar login
       try {
-          console.log('ENTRE A LA FUNCION')
+        console.log('ENTRE A LA FUNCION');
         const response = await fetch('REEMPLAZAR CON LOGIN', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({ nickname, contrasenia }),
-          
         });
-        console.log('Nickname:', nickname);
-        console.log('Contraseña:', contrasenia);
         const data = await response.json();
-      
-  
+    
         if (data.success) {
-          Alert.alert('Login exitoso', 'Se accedio correctamente al sitio.');
+          Alert.alert('Login exitoso', 'Se accedió correctamente al sitio.');
         } else if (data.error) {
           Alert.alert('Error de login', data.error);
-        }  else {
-          // Otro error
+        } else {
           Alert.alert('Error', 'Ocurrió un error al procesar el registro. Inténtalo nuevamente más tarde.');
         }
-        
-    } catch (error) {
+      } catch (error) {
         console.log(error);
         //Alert.alert('Error', 'Ocurrió un error al procesar el registro. Inténtalo nuevamente más tarde.');
       }
-      navigation.navigate('BottomTab')
-      console.log('TERMINE EL PROCESO DE REGISTRACION')
+    
+      console.log('Nickname:', nickname);
+      console.log('Contraseña:', contrasenia);
+      navigation.navigate('BottomTab');
+      console.log('TERMINE EL PROCESO DE INICIO DE SESION');
     };
   
   return (
     <View style={{ flex:1}}>
-    <ScrollView>
+    <ScrollView style={{flexGrow: 1, backgroundColor:'#FFF0B7'}}>
+
         {/*container - logo */}
-        <View style={{backgroundColor: '#ffffff'}}>
-            <View style={{backgroundColor: '#6874C3', padding: 65, borderBottomLeftRadius: 35}}>
+        <View style={{backgroundColor: '#FFFDC3'}}>
+            <View style={{backgroundColor: '#9C67A2', padding: 65, borderBottomLeftRadius: 35}}>
                 
                 <View style={{justifyContent: 'center', alignItems:'center'}}>
                     <Image Style={{width:100, height: 100, resizeMode:'contain'}}/>
@@ -61,22 +59,15 @@ const Login = () => {
             </View>
         </View>
         {/*container - login*/}
-        <View style={{backgroundColor: '#6874C3'}}>
-            <View style={{justifyContent: 'center', backgroundColor: '#ffffff', paddingHorizontal: 30, borderTopRightRadius: 35}}>
+        <View style={{backgroundColor: '#9C67A2'}}>
+            <View style={{justifyContent: 'center', backgroundColor: '#FFFED3', paddingHorizontal: 30, borderTopRightRadius: 35}}>
                 
                 <View style={{marginTop: 40, textAlign: 'center', display:'flex', alignItems:'center', justifyContent:'center'}}>
-                    <Text style={{fontWeight: '600', paddingLeft: 5, fontSize: 26, color: '#4f5898', padding:20}}>Username</Text>
+                    <Text style={{fontWeight: '600', paddingLeft: 5, fontSize: 26, color: '#803269', padding:20}}>Username</Text>
                     <TextInput
-                        style={{
-                        fontSize: 20,
-                        textAlign: 'center',
-                        width: 300,
-                        height: 40,
-                        margin: 5,
-                        borderRadius: 100,
-                        color: '#244f37',
-                        backgroundColor: '#e7e7e7',
-                        padding: 10,
+                       style={{
+                        fontSize: 20, textAlign: 'center', width: 300, height: 40, margin: 5,
+                        borderRadius: 100, color: '#703701', backgroundColor: '#FFE5A6', padding: 10
                         }}
                         autoCapitalize="none"
                         autoCorrect={false}
@@ -87,45 +78,35 @@ const Login = () => {
                 </View>
 
                 <View style={{marginTop: 10, textAlign: 'center', display:'flex', alignItems:'center', justifyContent:'center'}}>
-                    <Text style={{fontWeight: '600', paddingLeft: 5, fontSize: 26, color: '#4f5898', padding:20}}>Password</Text>
+                    <Text style={{fontWeight: '600', paddingLeft: 5, fontSize: 26, color: '#803269', padding:20}}>Password</Text>
                     <TextInput
-                style={{
-                  fontSize: 20,
-                  textAlign: 'center',
-                  width: 300,
-                  height: 40,
-                  margin: 5,
-                  borderRadius: 100,
-                  color: '#244f37',
-                  backgroundColor: '#e7e7e7',
-                  padding: 10,
-                }}
-                autoCapitalize="none"
-                autoCorrect={false}
-                value={contrasenia}
-                onChangeText={(text) => setContrasenia(text)}
-                placeholder="Ingrese su contraseña"
-              />
+                    style={{
+                    fontSize: 20, textAlign: 'center', width: 300, height: 40, margin: 5,
+                    borderRadius: 100, color: '#703701', backgroundColor: '#FFE5A6', padding: 10
+                    }}
+                    autoCapitalize='none' secureTextEntry autoCorrect={false}
+                    placeholder='Ingrese su contraseña'
+                    value={contrasenia}
+                    onChangeText={text => setContrasenia(text)}
+                />
                 </View>
 
-               
-                <View style={{display:'flex', marginTop: 20, alignItems:'center', justifyContent:'center'}}>
-                    <TouchableOpacity onPress={handleLogin}>
+              <View style={{ display: 'flex', marginTop: 35, alignItems: 'center', justifyContent: 'center' }}>
+              <TouchableOpacity onPress={handleLogin}>
+                  <Image style ={{width: 180, height: 35}} source={rodillo} />
 
-                    <View style={{margin: 5, backgroundColor: '#4f5898', borderRadius: 100, paddingVertical: 10, textAlign:'center', width:300}}>
-                        <Image source={require('./ruta/imagen.png')} />
-                    </View>
-                    </TouchableOpacity>
-                </View>
+              </TouchableOpacity>
+              </View>     
 
-                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <View style={{ paddingVertical: 20, justifyContent: 'center', alignItems: 'center'}}>
                     <TouchableOpacity onPress={() => navigation.navigate('RegisterStage1')}>
-                    <Text style={{fontWeight: '300', paddingLeft: 5, fontSize: 17, color: '#0A5269'}}><br></br>No tienes una cuenta?</Text>
+                    <Text style={{fontWeight: '300', paddingLeft: 5, fontSize: 17, color: '#713700'}}><br></br>No tienes una cuenta?</Text>
                     </TouchableOpacity>
                 </View>
- 
+              </View> 
             </View>
-        </View>
+          
+
         </ScrollView>
     </View>
     
