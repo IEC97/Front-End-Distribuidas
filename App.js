b492b1f09536b2cd28d0f5777b5996f8f40e2a1e
gianpeccia <giann.peccia@gmail.com>
2023-06-26 10:31:24 -0300

    act rama

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import LogIn from './src/components/LogIn.js'
 import BottomTab from './src/components/BottomTab.js'
+import ListaIngredientes from './src/components/ListaIngredientes.js';
+import ListaUnidades from './src/components/ListaUnidades.js';
 import RegisterStage1 from './src/components/RegisterStage1.js'
 import RegisterStage2 from './src/components/RegisterStage2.js';
 import Perfil from './src/screen/Perfil.js';
 import {NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
-
-
-// Crea una instancia de StackNavigator
 const Stack = createStackNavigator();
 
 const App = () => {
@@ -18,24 +16,15 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LogIn} options={{ headerShown: false}}/>
+        <Stack.Screen name="BottomTab" component={BottomTab} options={{ headerShown: false}}/>
+        <Stack.Screen name="ListaIngredientes" component={ListaIngredientes} options={{ headerShown: false}}/>
+        <Stack.Screen name="ListaUnidades" component={ListaUnidades} options={{ headerShown: false}}/>
         <Stack.Screen name="RegisterStage1" component={RegisterStage1} options={{ headerShown: false}}/>
         <Stack.Screen name="RegisterStage2" component={RegisterStage2} options={{ headerShown: false}}/>
-        <Stack.Screen name="BottomTab" component={BottomTab} options={{ headerShown: false}}/>
         <Stack.Screen name="Perfil" component={Perfil} options={{ headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
-  );
-  }
-
+  ); 
+}
 
 export default App;
-/*
-export default function App() {
-  return (
-    <LogIn/>
-    //<EditarRecetas/>
-    // <NavigationContainer>
-    //     <BottomTab/>
-    // </NavigationContainer> 
-  );
-};*/
--- src/components/ListaIngredientes.js
+++ src/components/ListaIngredientes.js
@@ -1,129 +1,236 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { Dimensions, FlatList, Picker, ScrollView, StyleSheet, Text, TouchableOpacity, View, TextInput, Na } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-const opciones = [
-  { id: 1, label: 'Opción 1' },
-  { id: 2, label: 'Opción 2' },
-  { id: 3, label: 'Opción 3' },
-  { id: 4, label: 'Opción 4' },
-];
+const categoriasIngredientes = {
+  'Verduras': ['Zanahoria', 'Cebolla', 'Tomate', 'Lechuga', 'Morron', 'Espinaca', 'Brócoli'],
+  'Carnes y pescados': ['Pollo', 'Vaca', 'Cerdo', 'Salmón', 'Merluza', 'Camarones'],
+  'Lácteos y productos lácteos': ['Leche', 'Leche condensada', 'Leche de almendras', 'Leche de coco', 'Leche de soja', 'Crema de leche', 'Queso crema', 'Queso azul', 'Queso mozzarella', 'Yogurt'],
+  'Huevos': ['Huevos frescos', 'Clara de huevo', 'Yema de huevo'],
+  'Legumbres': ['Arvejas', 'Lentejas', 'Garbanzos'],
+  'Frutas': ['Manzana', 'Banana', 'Naranja', 'Frutilla', 'Uva', 'Ananá', 'Mango'],
+  'Hierbas y especias': ['Albahaca', 'Orégano', 'Perejil', 'Cilantro', 'Cúrcuma', 'Comino', 'Canela'],
+  'Condimentos y salsas': ['Sal', 'Pimienta', 'Salsa de soja', 'Dulce de leche', 'Ketchup', 'Mostaza', 'Vinagre', 'Barbacoa', 'Mayonesa', 'Miel'],
+  'Aceites y grasas': ['Aceite de oliva', 'Aceite de girasol', 'Manteca', 'Margarina'],
+  'Frutos secos y semillas': ['Almendras', 'Nueces', 'Semillas de chía', 'Arroz', 'Quinoa', 'Avena', 'Semillas de girasol', 'Semillas de sésamo'],
+  'Harinas y panadería': ['Harina de trigo', 'Harina de maíz', 'Levadura', 'Azúcar'],
+  'Azúcares y endulzantes': ['Azúcar blanco', 'Azúcar negro', 'Stevia', 'Splenda'],
+  'Bebidas': ['Agua', 'Café', 'Té', 'Jugo de naranja', 'Vino tinto', 'Vino blanco'],
+  'Ingredientes enlatados y en conserva': ['Atún', 'Choclo', 'Aceitunas', 'Champiñones'],
+};
+const windowWidth = Dimensions.get('window').width;
 
-const ListaIngredientes =  () => {
-  const [opcionesSeleccionadas, setOpcionesSeleccionadas] = useState([]);
-  const navigation = useNavigation();
-  //navigation.navigate('Login');
+const ListaIngredientes = () => {
+    const navigation = useNavigation();
+    const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('');
+    const [ingredientes, setIngredientes] = useState([]);
+    const [ingredientesSeleccionados, setIngredientesSeleccionados] = useState(
+      new Set()
+    );
+    
 
-  const handleSeleccionarOpcion = (opcion) => {
-    const opcionIndex = opcionesSeleccionadas.findIndex((o) => o.id === opcion.id);
-
-    if (opcionIndex !== -1) {
-      setOpcionesSeleccionadas(prevSeleccionadas =>
-        prevSeleccionadas.filter((o) => o.id !== opcion.id)
+    const mostrarIngredientes = (categoria) => {
+      setCategoriaSeleccionada(categoria);
+      setIngredientes(categoriasIngredientes[categoria]);
+    };
+  
+    const seleccionarIngrediente = (ingrediente) => {
+      setIngredientesSeleccionados((prevSeleccionados) => {
+        const nuevosSeleccionados = new Set(prevSeleccionados);
+  
+        // Si el ingrediente ya está seleccionado, se deselecciona
+        if (nuevosSeleccionados.has(ingrediente)) {
+          nuevosSeleccionados.delete(ingrediente);
+        } else {
+          // Si el ingrediente no está seleccionado, se selecciona
+          nuevosSeleccionados.add(ingrediente);
+        }
+  
+        return nuevosSeleccionados;
+      });
+    };
+  
+    const renderizarIngredientes = ({ item }) => (
+        <TouchableOpacity
+          style={[styles.ingredienteButton, ingredientesSeleccionados.has(item) && styles.ingredienteButtonSelected]}
+          onPress={() => seleccionarIngrediente(item)}
+        >
+          <Text style={styles.ingredienteText}>{item}</Text>
+        </TouchableOpacity>
       );
-    } else {
-      setOpcionesSeleccionadas(prevSeleccionadas => [...prevSeleccionadas, opcion]);
-    }
-  };
+  
+    const renderizarCategorias = () => {
+        const categoriasOrdenadas = Object.keys(categoriasIngredientes).sort();
+        return categoriasOrdenadas.map((categoria) => (
+          <Picker.Item key={categoria} label={categoria} value={categoria} />
+        ));
+      };
+  
+    const renderizarIngredientesPorFila = ({ item }) => {
+      const ingredientesPorFila = [];
+      for (let i = 0; i < item.length; i += 1) {
+        const fila = item.slice(i, i + 1);
+        ingredientesPorFila.push(fila);
+      }
+  
+      return (
+        <View style={styles.ingredientesContainer}>
+          {ingredientesPorFila.map((fila, index) => (
+            <View key={index} >
+              {fila.map((ingrediente) => (
+                <TouchableOpacity
+                  key={ingrediente}
+                  style={[
+                    styles.ingredienteButton,
+                    ingredientesSeleccionados.has(ingrediente) &&
+                      styles.ingredienteButtonSelected,
+                  ]}
+                  onPress={() => seleccionarIngrediente(ingrediente)}
+                >
+                  <Text style={styles.ingredienteText}>{ingrediente}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          ))}
+        </View>
+      );
+    };
+  
+    return (
+      <View>
+        <ScrollView contentContainerStyle={styles.container}>
+          <TouchableOpacity
+            style={styles.buttonBack}
+            onPress={() => navigation.goBack()}
+          >
+            <Text style={styles.buttonText}>Volver</Text>
+          </TouchableOpacity>
+  
+          <Text style={styles.title}>Seleccionar los ingredientes</Text>
+          <Text style={styles.label}>Elige una categoría:</Text>
+          <Picker
+            selectedValue={categoriaSeleccionada}
+            onValueChange={mostrarIngredientes}
+            style={styles.picker}
+          >
+            <Picker.Item label="-- Seleccionar --" value="" />
+            {renderizarCategorias()}
+          </Picker>
+  
+          <Text style={styles.title}>Ingredientes:</Text>
+          <FlatList
+            data={ingredientes}
+            keyExtractor={(item) => item}
+            renderItem={renderizarIngredientes}
+            numColumns={3}
+          />
+  
+          <Text style={styles.title}>Ingredientes seleccionados:</Text>
+          <View style={styles.ingredientesSeleccionadosContainer}>
+            <FlatList
+              data={[Array.from(ingredientesSeleccionados)]}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={renderizarIngredientesPorFila}
+            />
+          </View>
 
-  return (
-      <View style={{ flex: 1 }}>
-          <ScrollView contentContainerStyle={styles.container}>
-              <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
-                <Text style={styles.buttonText}>Volver</Text>
-              </TouchableOpacity>
-              <View>
-                  <Text style={styles.titulo}>Seleccionar ingredientes</Text>
-              </View>
+          <View style={{textAlign: 'center', padding: 30}}>
+                <Text style={{fontSize: 15}}>Alguno de los ingredientes que usas no está en la lista?
+                Añadilo acá!</Text>
+                    
+                <View>
+                    <TextInput
+                    style={{
+                        fontSize: 15, alignSelf: 'center',textAlign: 'center', width: 200, height: 30, margin: 5, 
+                        borderRadius: 100, color: 'black', backgroundColor: '#f7dbb2', padding: 10
+                        }}
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        placeholder="Ingrese otro ingrediente"
+                    />
+                </View>
+            </View>
 
-              {/* Opciones */}
-              <View>
-                {opciones.map((opcion) => (
-                  <TouchableOpacity
-                    key={opcion.id}
-                    onPress={() => handleSeleccionarOpcion(opcion)}
-                    style={[
-                      styles.optionButton,
-                      opcionesSeleccionadas.some((o) => o.id === opcion.id) && styles.optionButtonSelected,
-                    ]}
-                  >
-                    <Text>{opcion.label}</Text>
-                  </TouchableOpacity>
-                ))}
-                </View>
-
-                <View style={{marginTop: 10}}>
-                  <Text style={styles.titulo}>Opciones seleccionadas:</Text>
-                  {opcionesSeleccionadas.map((opcion) => (
-                    <Text style={{textAlign: 'center'}} key={opcion.id}>{opcion.label}</Text>
-                  ))}
-                </View>
-                
-                <View style={{textAlign: 'center', padding: 30}}>
-                    <Text style={{fontSize: 15}}>Alguno de los ingredientes que usas no está en la lista?
-                    Añadilo acá abajo y lo revisaremos!</Text>
-                      
-                    <View>
-                      <TextInput
-                        style={{
-                          fontSize: 15, alignSelf: 'center',textAlign: 'center', width: 200, height: 30, margin: 5, 
-                          borderRadius: 100, color: '#703701', backgroundColor: '#FFE5A6', padding: 10
-                          }}
-                          autoCapitalize="none"
-                          autoCorrect={false}
-                          placeholder="Ingrese otro ingrediente"
-                      />
-                    </View>
-                </View>
-       
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('ListaUnidades')}>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('ListaUnidades')}>
                 <Text style={styles.buttonText}>Continuar</Text>
             </TouchableOpacity>
-            <Text style={{paddingTop: 10, fontSize: 12, fontStyle: 'italic'}}>Continuar con la seleccion de unidades por ingrediente.</Text>
-
-          </ScrollView>
+            <Text style={{paddingBottom: 20, fontSize: 12, fontStyle: 'italic'}}>Continuar con la seleccion de unidades por ingrediente.</Text>
+        </ScrollView>
+        
       </View>
     );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 20,
-    backgroundColor:'#FFFED3',
-  },
-  titulo: {
-    margin: 10,
-    color: 'black',
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  button: {
-    marginTop: 10,
-    backgroundColor: '#703701',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 100,
-    paddingVertical: 5,
-    paddingHorizontal: 20,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 15,
-  },
-  optionButton: {
-    fontSize: 15, textAlign: 'center', width: 100, height: 20, margin: 5, 
-    borderRadius: 100, color: '#703701', backgroundColor: '#FFE5A6',
-  },
-  optionButtonSelected: {
-    backgroundColor: '#c2780a',
-  },
-  optionButtonText: {
-    fontWeight: 'bold',
-  },
- 
-});
-
-export default ListaIngredientes;
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#FFFED3',
+    },
+    button: {
+        backgroundColor: '#703701',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 20,
+    },
+    buttonBack: {
+        marginRight: 'auto',
+        backgroundColor: '#703701',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 20,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 5,
+    },
+    picker: {
+        width: windowWidth - 40,
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    ingredienteButton: {
+        backgroundColor: '#E66262',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 10,
+        marginRight: 10,
+    },
+    ingredienteButtonSelected: {
+        backgroundColor: '#FAB3B3',
+    },
+    ingredienteText: {
+        fontSize: 16,
+        color: 'black',
+    },
+    ingredientesSeleccionadosContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginHorizontal: 35,
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    ingredientesContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        flex:1,
+    },
+    filaContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginBottom: 5,
+    },
+  });
+  
+  export default ListaIngredientes;
