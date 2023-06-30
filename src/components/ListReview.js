d25027668fc0b82a8696dd76fea74f84a04521e0
Ivana Cardozo <iva6869@gmail.com>
2023-06-30 18:13:35 -0300

    Agregando ListReview

---
+++ src/components/ListReview.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ListReview=({})=> {
+  return (
+    <View>
+      <Text>ListReview</Text>
+    </View>
+  )
+}
+export default ListReview;
+const styles = StyleSheet.create({})
